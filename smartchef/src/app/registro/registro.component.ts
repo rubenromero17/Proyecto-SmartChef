52017c6a09dbff0c14118fd2fb1d7bc2698dca3e
rubenromero17 <ruromeroaponte@safareyes.es>
2025-11-06 13:21:53 +0100

    Segundo Avance creamos registro

---
+++ smartchef/src/app/registro/registro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.scss'],
+  standalone: true,
+})
+export class RegistroComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
