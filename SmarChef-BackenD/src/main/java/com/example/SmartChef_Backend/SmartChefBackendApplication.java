7ba9ea458e59f4bdda8571b119fb4d3ea99385c1
rubenromero17 <ruromeroaponte@safareyes.es>
2025-11-20 09:17:20 +0100

    Backend

---
+++ SmarChef-BackenD/src/main/java/com/example/SmartChef_Backend/SmartChefBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartChef_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartChefBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartChefBackendApplication.class, args);
+	}
+
+}
---
+++ SmarChef-BackenD/src/test/java/com/example/SmartChef_Backend/SmartChefBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartChef_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartChefBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
