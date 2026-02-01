579a4b0d8798450ab3a082e6868480d4a6ca3c2c
rubenromero17 <ruromeroaponte@safareyes.es>
2026-02-01 21:16:08 +0100

    nada

--- smartchef/src/environments/environment.ts
+++ smartchef/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  apiUrl: 'https://proyecto-smartchef.onrender.com' // Tu URL de backend en Render
 };
 
 /*
